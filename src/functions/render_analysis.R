199042f3df04941811b16e6d375cca79eb530a55
ava <avakiai@gmail.com>
2023-02-23 16:25:26 +0100

    fix path issues

--- src/functions/render_analysis.R
+++ src/functions/render_analysis.R
@@ -1,8 +1,13 @@
 
-render_analysis <- function(exp, plot_sm, use_all, data_path, results_path, seed) {
+render_analysis <- function(exp = "exp_1", 
+                            plot_sm = TRUE, 
+                            use_all = TRUE, 
+                            data_path = './../1-data', 
+                            results_path = './../3-results', 
+                            seed = 42) {
   rmarkdown::render(
-    './../R/2-analysis/master_analysis.Rmd',
-    output_file = paste0('./../vignettes/master-analyses', '_', gsub("_","-",exp), '_', gsub(" ","_",gsub(":","-", Sys.time())), '.html'),
+    './R/2-analysis/master_analysis.Rmd',
+    output_file = paste0('./../../vignettes/master-analyses', '_', gsub("_","-",exp), '_', gsub(" ","_",gsub(":","-", Sys.time())), '.html'),
     params = list(data_path = data_path, 
                   results_path = results_path,
                   exp = exp, 
